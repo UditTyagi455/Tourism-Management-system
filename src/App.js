2212ed51078ffe124b6955406468471700cb30de
udit tyagi <udittyagi455@gmail.com>
2021-12-19 15:27:41 +0530

    first commit in udit branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import First from './component/First';
 
 function App() {
   return (
     <div className="App">
       <h1>Tourism Management System</h1>
+      <First/>
     </div>
   );
 }
---
+++ src/component/First.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const First = () => {
+    return (
+        <div>
+            <h2>This is First created component.</h2>
+        </div>
+    )
+}
+
+export default First
